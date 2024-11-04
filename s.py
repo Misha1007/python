beaedb078336736636ec21e0f63969dad9025eda
My Name <moroz-off2002@mail.ru>
2024-11-04 18:39:35 +0300

    last

--- s.py
+++ s.py
@@ -8,9 +8,10 @@ WHITE = pygame.Color(255, 255, 255)
 screen.fill(WHITE)
 
 x=0
-y1=0
 y=0
-i=0
+z=400
+t=0
+p=200
 
 while True:
     for event in pygame.event.get():
@@ -19,28 +20,28 @@ while True:
             sys.exit()
 
     y+=1
-    if y==50:
-        for i in range (250, 0, -1):   
+    if y>50:
+        for i in range (250, 0, -1):
             y-=1
-
-        y1-=1
-        if y1==0:
-                for i in range(, 1):
-                    pygame.draw.rect(screen, (255, 255, 255), (250, 250, 50, y1))#прямоугольник Y
-
+            pygame.draw.rect(screen, (255, 255, 255), (250, y, 50, 200))#прямоугольник Y
     x+=1
     if x>50:
-        x=0
+        for j in range (250, 0, -1):
+            x-=1
+            pygame.draw.rect(screen, (255, 255, 255), (x, 300, 200, 50))#прямоугольник X
     z=-1
-    if z>50:
-        z=600
+    if z>250:
+        for l in range(400, 600, 1):
+            for p in range(200, 250, 1):
+                z+=1
+            pygame.draw.rect(screen, (255, 255, 255), (300, z, 50, p))#прямоугольник Z
     t=-1
     if t>50:
         t=600
     pygame.draw.rect(screen, (255, 255, 0), (250, 250, 100, 100))#квадрат
     pygame.draw.rect(screen, (255, 0, 0), (250, y, 50, 200))#прямоугольник Y
     pygame.draw.rect(screen, (255, 0, 0), (x, 300, 200, 50))#прямоугольник X
-    pygame.draw.rect(screen, (255, 0, 255), (300, z, 200, 50))#прямоугольник Z
+    pygame.draw.rect(screen, (255, 0, 0), (300, z, 50, p))#прямоугольник Z
     pygame.draw.rect(screen, (255, 0, 255), (t, 250, 50, 200))#прямоугольник T
     pygame.display.update()
 
